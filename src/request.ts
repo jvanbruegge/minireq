ff552ec614ad5eb51a842085d400f35cef6e8eb3
Jan van Brügge <supermanitu@gmail.com>
2020-02-11 21:18:58 +0100

    Add serialization tests

--- src/request.ts
+++ src/request.ts
@@ -29,88 +29,92 @@ export function makeRequest(
         const opts = { ...defaults, ...defaultOptions, ...options };
         const url = opts.url + (opts.query ? makeQueryString(opts.query) : '');
 
-        const promise = new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
+        // Because fuck JavaScript Promises and their garbage error handing regarding throw
+        let resolve: any;
+        let reject: any;
 
-            abort = () => {
-                request.abort();
-            };
+        const request = new XMLHttpRequest();
 
-            request.addEventListener('load', () => {
-                let response: any = request.response;
-                if (request.responseType === 'text') {
-                    const mimeType = request
-                        .getResponseHeader('Content-Type')
-                        ?.split(';')[0];
-                    if (mimeType && serializers[mimeType]) {
-                        response = serializers[mimeType].parse(response);
-                    }
+        abort = () => {
+            request.abort();
+        };
+
+        request.addEventListener('load', () => {
+            let response: any = request.response;
+            if (request.responseType === 'text') {
+                const mimeType = request
+                    .getResponseHeader('Content-Type')
+                    ?.split(';')[0];
+                if (mimeType && serializers[mimeType]) {
+                    response = serializers[mimeType].parse(response);
                 }
+            }
 
-                resolve({
-                    status: request.status,
-                    data: response
-                });
+            resolve({
+                status: request.status,
+                data: response
             });
-            request.addEventListener('error', reject);
-
-            request.open(
-                opts.method,
-                url,
-                true,
-                opts.auth?.user,
-                opts.auth?.password
-            );
-
-            request.responseType = opts.responseType;
-
-            if (opts.headers) {
-                for (const key in opts.headers) {
-                    request.setRequestHeader(key, opts.headers[key]);
-                }
-            }
-            request.setRequestHeader('Content-Type', opts.contentType);
-
-            if (opts.progress) {
-                request.addEventListener('progress', opts.progress);
-            }
-
-            if (opts.send) {
-                if (
-                    typeof opts.send === 'string' ||
-                    opts.send instanceof Blob ||
-                    opts.send instanceof ArrayBuffer ||
-                    opts.send instanceof Int8Array ||
-                    opts.send instanceof Uint8Array ||
-                    opts.send instanceof Uint8ClampedArray ||
-                    opts.send instanceof Int16Array ||
-                    opts.send instanceof Uint16Array ||
-                    opts.send instanceof Int32Array ||
-                    opts.send instanceof Uint32Array ||
-                    opts.send instanceof Float32Array ||
-                    opts.send instanceof Float64Array ||
-                    opts.send instanceof DataView ||
-                    opts.send instanceof FormData ||
-                    opts.send instanceof URLSearchParams
-                ) {
-                    request.send(opts.send);
-                } else {
-                    if (serializers[opts.contentType]) {
-                        request.send(
-                            serializers[opts.contentType].convert(opts.send)
-                        );
-                    } else {
-                        throw new Error(
-                            `Could not find a serializer for content type ${opts.contentType}`
-                        );
-                    }
-                }
-            } else {
-                request.send();
-            }
         });
+        request.addEventListener('error', reject);
 
-        return { promise: promise as any, abort };
+        request.open(
+            opts.method,
+            url,
+            true,
+            opts.auth?.user,
+            opts.auth?.password
+        );
+
+        request.responseType = opts.responseType;
+
+        if (opts.headers) {
+            for (const key in opts.headers) {
+                request.setRequestHeader(key, opts.headers[key]);
+            }
+        }
+        request.setRequestHeader('Content-Type', opts.contentType);
+
+        if (opts.progress) {
+            request.addEventListener('progress', opts.progress);
+        }
+
+        if (opts.send) {
+            if (
+                typeof opts.send === 'string' ||
+                opts.send instanceof Blob ||
+                opts.send instanceof ArrayBuffer ||
+                opts.send instanceof Int8Array ||
+                opts.send instanceof Uint8Array ||
+                opts.send instanceof Uint8ClampedArray ||
+                opts.send instanceof Int16Array ||
+                opts.send instanceof Uint16Array ||
+                opts.send instanceof Int32Array ||
+                opts.send instanceof Uint32Array ||
+                opts.send instanceof Float32Array ||
+                opts.send instanceof Float64Array ||
+                opts.send instanceof DataView ||
+                opts.send instanceof FormData ||
+                opts.send instanceof URLSearchParams
+            ) {
+                request.send(opts.send);
+            } else if (serializers[opts.contentType]) {
+                request.send(serializers[opts.contentType].convert(opts.send));
+            } else {
+                throw new Error(
+                    `Could not find a serializer for content type ${opts.contentType}`
+                );
+            }
+        } else {
+            request.send();
+        }
+
+        return {
+            promise: new Promise<any>((res, rej) => {
+                resolve = res;
+                reject = rej;
+            }),
+            abort
+        };
     };
 }
 
---
+++ test/send.ts
@@ -0,0 +1,42 @@
+import * as assert from 'assert';
+import { url, resetDB } from './helpers';
+
+import { makeRequest } from '../src/index';
+
+describe('send tests', () => {
+    const request = makeRequest();
+
+    afterEach(resetDB);
+
+    it('should send strings without serializing first', () => {
+        const str = JSON.stringify({
+            name: 'Hans',
+            age: 99,
+            children: []
+        });
+
+        const { promise } = request({
+            method: 'POST',
+            url: url('/users'),
+            send: str
+        });
+
+        return promise.then(({ status, data }) => {
+            assert.strictEqual(status, 201);
+            assert.deepStrictEqual(data, { ...JSON.parse(str), id: 4 });
+        });
+    });
+
+    it('should throw error if passed an object without serializer', () => {
+        assert.throws(() =>
+            request({
+                method: 'POST',
+                url: url('/users'),
+                send: {
+                    foo: 'bar'
+                },
+                contentType: 'application/xml'
+            })
+        );
+    });
+});
